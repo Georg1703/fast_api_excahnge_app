d66be6fdca8d39d9a1df105149c0646c21af5c7d
Gheorghe Mirca <gheorghemirca@Gheorghes-MacBook-Pro.local>
2024-02-23 19:42:40 +0200

    simplified data due to the external api limitations

--- app/data/currencies.py
+++ app/data/currencies.py
@@ -1,6 +1,4 @@
 currencies = [
     {"code": "USD", "name": "US Dollar"},
     {"code": "EUR", "name": "Euro"},
-    {"code": "RUB", "name": "Russian Ruble"},
-    # {"code": "UAH", "name": "Hryvnia "},
 ]
--- app/data/exchange_rates.py
+++ app/data/exchange_rates.py
@@ -1,8 +1,4 @@
 exchange_rates = [
     {"from_currency_id": 1, "to_currency_id": 2, "rate": 1},
-    {"from_currency_id": 1, "to_currency_id": 3, "rate": 1},
     {"from_currency_id": 2, "to_currency_id": 1, "rate": 1},
-    {"from_currency_id": 2, "to_currency_id": 3, "rate": 1},
-    {"from_currency_id": 3, "to_currency_id": 1, "rate": 1},
-    {"from_currency_id": 3, "to_currency_id": 2, "rate": 1},
 ]
